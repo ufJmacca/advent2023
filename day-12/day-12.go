593cd5c1fbdd2408a7361d162d1cf4cdca0e357f
ufJmacca <nitro.jmacca@gmail.com>
2023-12-13 09:31:19 +0000

    ADD: TestArraangementCounted passing

--- day-12/day-12.go
+++ day-12/day-12.go
@@ -1,8 +1,44 @@
 package main
 
-func ArrangementCounter(records string, conditions []int) int {
+import (
+	"strings"
+)
 
-	return 0
+func ArrangementCounter(records string, conditions []int) int {
+	if records == "" {
+		if len(conditions) == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if len(conditions) == 0 {
+		if strings.Contains(records, `#`) {
+			return 0
+		} else {
+			return 1
+		}
+	}
+
+	result := 0
+
+	if strings.Contains(".?", string(records[0])) {
+		result += ArrangementCounter(records[1:], conditions)
+	}
+
+	if strings.Contains("#?", string(records[0])) {
+		if conditions[0] <= len(records) && !strings.Contains(records[:conditions[0]], ".") && (conditions[0] == len(records) || string(records[conditions[0]]) != "#") {
+			if conditions[0] == len(records) {
+				result += ArrangementCounter("", conditions[1:])
+			} else {
+				result += ArrangementCounter(records[conditions[0]+1:], conditions[1:])
+			}
+
+		}
+	}
+
+	return result
 }
 
 func Puzzle1(input string) int {
