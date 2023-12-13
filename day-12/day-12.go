a63cc9abecfdfeba5219497250ec3532dccd3c8a
ufJmacca <nitro.jmacca@gmail.com>
2023-12-13 10:38:41 +0000

    ADD: memorization for part 2 performance

--- day-12/day-12.go
+++ day-12/day-12.go
@@ -9,6 +9,17 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type memo_key struct {
+	records    string
+	conditions []int
+}
+
+func StringifyMemoKey(key memo_key) string {
+	return fmt.Sprintf("%s:%v", key.records, key.conditions)
+}
+
+var memo = make(map[string]int)
+
 func ArrangementCounter(records string, conditions []int) int {
 	if records == "" {
 		if len(conditions) == 0 {
@@ -24,6 +35,11 @@ func ArrangementCounter(records string, conditions []int) int {
 		} else {
 			return 1
 		}
+	}
+
+	key := StringifyMemoKey(memo_key{records: records, conditions: conditions})
+	if val, ok := memo[key]; ok {
+		return val
 	}
 
 	result := 0
@@ -43,6 +59,7 @@ func ArrangementCounter(records string, conditions []int) int {
 		}
 	}
 
+	memo[key] = result
 	return result
 }
 
@@ -110,8 +127,8 @@ func main() {
 		puzzle_1 := Puzzle1(inputs)
 		fmt.Println(puzzle_1)
 
-		// puzzle_2 := Puzzle2(inputs)
-		// fmt.Println(puzzle_2)
+		puzzle_2 := Puzzle2(inputs)
+		fmt.Println(puzzle_2)
 	})
 
 	c.Visit("https://adventofcode.com/2023/day/12/input")
