6d302ae8287211daf480a60e1d8d2a9baf65aac5
ufJmacca <nitro.jmacca@gmail.com>
2023-12-14 11:31:23 +0000

    ADD: day 14 test cases added for puzzle input and north tilt

---
+++ day-14/day-14.go
@@ -0,0 +1,11 @@
+package main
+
+func NorthTilt([][]string) [][]string {
+	var result [][]string
+
+	return result
+}
+
+func Puzzle1(input string) int {
+	return 0
+}
---
+++ day-14/day-14_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type puzzle_input_1 struct {
+	input  string
+	result int
+}
+
+type platform struct {
+	input  [][]string
+	result [][]string
+}
+
+func TestPuzzle1(t *testing.T) {
+	test_data := []puzzle_input_1{
+		{`O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`, 136},
+	}
+
+	for _, datum := range test_data {
+		result := Puzzle1(datum.input)
+
+		if result != datum.result {
+			t.Errorf("Puzzle1(%s) FAILED - Expected %d Got %d\n", datum.input, datum.result, result)
+		} else {
+			t.Logf("Puzzle1(%s) PASSED", datum.input)
+		}
+	}
+}
+
+func TestNorthTilt(t *testing.T) {
+	test_data := []platform{
+		{[][]string{
+			{`O`, `.`, `.`, `.`, `.`, `#`, `.`, `.`, `.`, `.`},
+			{`O`, `.`, `O`, `O`, `#`, `.`, `.`, `.`, `.`, `#`},
+			{`.`, `.`, `.`, `.`, `.`, `#`, `#`, `.`, `.`, `.`},
+			{`O`, `O`, `.`, `#`, `O`, `.`, `.`, `.`, `.`, `O`},
+			{`.`, `O`, `.`, `.`, `.`, `.`, `.`, `O`, `#`, `.`},
+			{`O`, `.`, `#`, `.`, `.`, `O`, `.`, `#`, `.`, `#`},
+			{`.`, `.`, `O`, `.`, `.`, `#`, `O`, `.`, `.`, `O`},
+			{`.`, `.`, `.`, `.`, `.`, `.`, `.`, `O`, `.`, `.`},
+			{`#`, `.`, `.`, `.`, `.`, `#`, `#`, `#`, `.`, `.`},
+			{`#`, `O`, `O`, `.`, `.`, `#`, `.`, `.`, `.`, `.`},
+		}, [][]string{
+			{`O`, `O`, `O`, `O`, `.`, `#`, `.`, `O`, `.`, `.`},
+			{`O`, `O`, `.`, `.`, `#`, `.`, `.`, `.`, `.`, `#`},
+			{`O`, `O`, `.`, `.`, `O`, `#`, `#`, `.`, `.`, `O`},
+			{`O`, `.`, `.`, `#`, `.`, `O`, `O`, `.`, `.`, `.`},
+			{`.`, `.`, `.`, `.`, `.`, `.`, `.`, `.`, `#`, `.`},
+			{`.`, `.`, `#`, `.`, `.`, `.`, `.`, `#`, `.`, `#`},
+			{`.`, `.`, `O`, `.`, `.`, `#`, `.`, `O`, `.`, `O`},
+			{`.`, `.`, `O`, `.`, `.`, `.`, `.`, `.`, `.`, `.`},
+			{`#`, `.`, `.`, `.`, `.`, `#`, `#`, `#`, `.`, `.`},
+			{`#`, `.`, `.`, `.`, `.`, `#`, `.`, `.`, `.`, `.`},
+		},
+		},
+	}
+
+	for _, datum := range test_data {
+		result := NorthTilt(datum.input)
+
+		correct := true
+
+		size := len(result)
+		if size > 0 {
+			for i := 0; i < size; i++ {
+				if !reflect.DeepEqual(result[i], datum.result[i]) {
+					correct = false
+				}
+			}
+		} else {
+			t.Errorf("NorthTilt(%s) FAILED - Empty result", datum.input)
+		}
+
+		if !correct {
+			t.Errorf("NorthTilt(%s) FAILED - Expect %s got %s", datum.input, datum.result, result)
+		} else if size > 0 {
+			t.Logf("NorthTilt(%s) PASSED", datum.input)
+		}
+	}
+}
