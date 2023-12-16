c0dd01f5b23ca4259de8983b745382b839f108bb
ufJmacca <nitro.jmacca@gmail.com>
2023-12-16 11:01:12 +0000

    ADD: Test Hash Algo passing

--- day-15/day-15.go
+++ day-15/day-15.go
@@ -1,8 +1,13 @@
 package main
 
 func Hash(input string) int {
+	current_value := 0
 
-	return 0
+	for _, char := range input {
+		current_value += int(byte(char))
+		current_value = (current_value * 17) % 256
+	}
+	return current_value
 }
 
 func Puzzle1(input string) int {
