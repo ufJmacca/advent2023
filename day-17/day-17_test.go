628190397b7450705400987ad0dfe26769eb6152
ufJmacca <nitro.jmacca@gmail.com>
2023-12-17 07:18:48 +0000

    ADD: day 17 part 1 test added

---
+++ day-17/day-17_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+type puzzle_input_1 struct {
+	input  string
+	result int
+}
+
+func TestPuzzle1(t *testing.T) {
+	test_data := []puzzle_input_1{
+		{`2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533`, 102},
+	}
+
+	for _, datum := range test_data {
+		result := Puzzle1(datum.input)
+
+		if result != datum.result {
+			t.Errorf("Puzzle1(%s) FAILED - Expected %d Got %d\n", datum.input, datum.result, result)
+		} else {
+			t.Logf("Puzzle1(%s) PASSED", datum.input)
+		}
+	}
+}
