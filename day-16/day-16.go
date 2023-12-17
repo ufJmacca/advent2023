73b4abfae5f5d70c541ae710e8044f867e1243e1
ufJmacca <nitro.jmacca@gmail.com>
2023-12-17 03:51:46 +0000

    ADD: day 16 puzzle 1 complete

--- day-16/day-16.go
+++ day-16/day-16.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 type grid_element struct {
@@ -29,7 +32,7 @@ func NextStep(input_beam beam, grid [][]grid_element, rows int, cols int) []beam
 	grid[input_beam.current_cell[0]][input_beam.current_cell[1]].visits++
 	if grid[input_beam.current_cell[0]][input_beam.current_cell[1]].visits < 100 {
 		next_x, next_y := input_beam.current_cell[0]+next_cell[input_beam.direction][0], input_beam.current_cell[1]+next_cell[input_beam.direction][1]
-		fmt.Printf("Current step (%d, %d) - direction %s\n", input_beam.current_cell[0], input_beam.current_cell[1], input_beam.direction)
+		// fmt.Printf("Current step (%d, %d) - direction %s\n", input_beam.current_cell[0], input_beam.current_cell[1], input_beam.direction)
 
 		if next_x >= 0 && next_x < rows && next_y >= 0 && next_y < cols {
 			switch input_beam.direction {
@@ -37,25 +40,25 @@ func NextStep(input_beam beam, grid [][]grid_element, rows int, cols int) []beam
 				next_step := grid[next_x][next_y]
 				if next_step.value == "|" {
 					// NORTH / SOUTH
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"north", []int{next_x, next_y}},
 						{"south", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "\\" {
 					// SOUTH
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"south", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "/" {
 					// NORTH
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"north", []int{next_x, next_y}},
 					}
 				} else {
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"east", []int{next_x, next_y}},
 					}
@@ -65,25 +68,25 @@ func NextStep(input_beam beam, grid [][]grid_element, rows int, cols int) []beam
 				next_step := grid[next_x][next_y]
 				if next_step.value == "|" {
 					// NORTH / SOUTH
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"north", []int{next_x, next_y}},
 						{"south", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "\\" {
 					// NORTH
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"north", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "/" {
 					// south
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"south", []int{next_x, next_y}},
 					}
 				} else {
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"west", []int{next_x, next_y}},
 					}
@@ -92,25 +95,25 @@ func NextStep(input_beam beam, grid [][]grid_element, rows int, cols int) []beam
 				next_step := grid[next_x][next_y]
 				if next_step.value == "-" {
 					// west/east
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"west", []int{next_x, next_y}},
 						{"east", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "\\" {
 					// west
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"west", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "/" {
 					// east
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"east", []int{next_x, next_y}},
 					}
 				} else {
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"north", []int{next_x, next_y}},
 					}
@@ -119,25 +122,25 @@ func NextStep(input_beam beam, grid [][]grid_element, rows int, cols int) []beam
 				next_step := grid[next_x][next_y]
 				if next_step.value == "-" {
 					// west/east
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"west", []int{next_x, next_y}},
 						{"east", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "\\" {
 					// west
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"east", []int{next_x, next_y}},
 					}
 				} else if next_step.value == "/" {
 					// east
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"west", []int{next_x, next_y}},
 					}
 				} else {
-					fmt.Println(next_step.value)
+					// fmt.Println(next_step.value)
 					return []beam{
 						{"south", []int{next_x, next_y}},
 					}
@@ -173,7 +176,11 @@ func Puzzle1(input string) int {
 	rows := len(grid)
 	cols := len(grid[0])
 
-	beams = append(beams, beam{direction: "east", current_cell: []int{0, 0}})
+	if strings.Contains("\\|", grid[0][0].value) {
+		beams = append(beams, beam{direction: "south", current_cell: []int{0, 0}})
+	} else {
+		beams = append(beams, beam{direction: "east", current_cell: []int{0, 0}})
+	}
 
 	for len(beams) > 0 {
 		next_step := NextStep(beams[0], grid, rows, cols)
@@ -185,13 +192,34 @@ func Puzzle1(input string) int {
 		for _, col := range row {
 			if col.energised {
 				count++
-				fmt.Printf("#")
+				// fmt.Printf("#")
 			} else {
-				fmt.Printf(".")
+				// fmt.Printf(".")
 			}
 		}
-		fmt.Printf("\n")
+		// fmt.Printf("\n")
 	}
 
 	return count
+}
+
+func main() {
+	c := colly.NewCollector()
+
+	// Sets cookie from environment variable
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("cookie", os.Getenv("COOKIE"))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		inputs := string(r.Body)
+
+		puzzle_1 := Puzzle1(inputs)
+		fmt.Println(puzzle_1)
+
+		// puzzle_2 := Puzzle2(inputs)
+		// fmt.Println(puzzle_2)
+	})
+
+	c.Visit("https://adventofcode.com/2023/day/16/input")
 }
--- day-16/day-16_test.go
+++ day-16/day-16_test.go
@@ -2,13 +2,38 @@ package main
 
 import "testing"
 
-type puzzle_input_1 struct {
+type puzzle_input struct {
 	input  string
 	result int
 }
 
 func TestPuzzle1(t *testing.T) {
-	test_data := []puzzle_input_1{
+	test_data := []puzzle_input{
+		{`.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....`, 46},
+	}
+
+	for _, datum := range test_data {
+		result := Puzzle1(datum.input)
+
+		if result != datum.result {
+			t.Errorf("Puzzle1(%s) FAILED - Expected %d Got %d\n", datum.input, datum.result, result)
+		} else {
+			t.Logf("Puzzle1(%s) PASSED", datum.input)
+		}
+	}
+}
+
+func TestPuzzle2(t *testing.T) {
+	test_data := []puzzle_input{
 		{`.|...\....
 |.-.\.....
 .....|-...
