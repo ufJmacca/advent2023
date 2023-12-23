ca81a3b59f403b9bf1a3353f2621c1bfcf92edf6
ufJmacca <nitro.jmacca@gmail.com>
2023-12-23 05:08:16 +0000

    ADD: Day 19 puzzle 2 test passed

--- day-19/day-19.go
+++ day-19/day-19.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -83,9 +84,98 @@ func Puzzle1(input string) int {
 	return total
 }
 
-func Puzzle2(input string) int {
+func CopyMap(originalMap map[string][]int) map[string][]int {
+	copiedMap := make(map[string][]int)
 
-	return 0
+	for key, value := range originalMap {
+		copiedSlice := make([]int, len(value))
+		copy(copiedSlice, value)
+
+		copiedMap[key] = copiedSlice
+	}
+
+	return copiedMap
+}
+
+func RangeEvaluator(part_range map[string][]int, workflow string, workflows map[string][]string) int {
+	if workflow == "A" {
+		sum := 1
+		for _, value := range part_range {
+			sum *= slices.Max(value) - slices.Min(value) + 1
+		}
+		return sum
+	}
+
+	if workflow == "R" {
+		return 0
+	}
+
+	total := 0
+	len_rules := len(workflows[workflow])
+	rules := workflows[workflow][:len_rules-1]
+	fallback := workflows[workflow][len_rules-1:][0]
+
+	for _, rule := range rules {
+		criteria := strings.Split(rule, ":")
+
+		var true_slice []int
+		var false_slice []int
+		var rule_split []string
+
+		if strings.Contains(criteria[0], "<") {
+			rule_split = strings.Split(criteria[0], "<")
+			value, _ := strconv.Atoi(rule_split[1])
+			true_slice = []int{part_range[rule_split[0]][0], (value - 1)}
+			false_slice = []int{value, part_range[rule_split[0]][1]}
+		} else {
+			rule_split = strings.Split(criteria[0], ">")
+			value, _ := strconv.Atoi(rule_split[1])
+			true_slice = []int{(value + 1), part_range[rule_split[0]][1]}
+			false_slice = []int{part_range[rule_split[0]][0], value}
+		}
+
+		if true_slice[0] <= true_slice[1] {
+			copy_range := CopyMap(part_range)
+			copy_range[rule_split[0]] = true_slice
+			total += RangeEvaluator(copy_range, criteria[1], workflows)
+		}
+		if false_slice[0] <= false_slice[1] {
+			part_range = CopyMap(part_range)
+			part_range[rule_split[0]] = false_slice
+		} else {
+			break
+		}
+	}
+	total += RangeEvaluator(part_range, fallback, workflows)
+
+	return total
+}
+
+func Puzzle2(input string) int {
+	blocks := strings.Split(input, "\n\n")
+
+	flows_regex := regexp.MustCompile(`(\w+){(.*?)}`)
+
+	flows := make(map[string][]string)
+
+	for _, line := range strings.Split(blocks[0], "\n") {
+		if len(line) > 0 {
+			matches := flows_regex.FindAllStringSubmatch(line, -1)
+			flows[matches[0][1]] = append(flows[matches[0][1]], strings.Split(matches[0][2], ",")...)
+		}
+	}
+
+	total := 0
+
+	product_ranges := make(map[string][]int)
+
+	for _, key := range []string{"x", "m", "a", "s"} {
+		product_ranges[key] = []int{1, 4000}
+	}
+
+	total += RangeEvaluator(product_ranges, "in", flows)
+
+	return total
 }
 
 func main() {
