1d24278f1846b9ca6d4e9499f666e90bd6e8e726
ufJmacca <nitro.jmacca@gmail.com>
2023-12-23 12:01:06 +0000

    ADD: day 20 part 1 complete

--- day-20/day-20.go
+++ day-20/day-20.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"regexp"
 	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 type module struct {
@@ -140,4 +144,26 @@ func Puzzle1(input string) int {
 	}
 
 	return low * high
+}
+
+func main() {
+	c := colly.NewCollector()
+
+	// Sets cookie from environment variable
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("cookie", os.Getenv("COOKIE"))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		inputs := string(r.Body)
+
+		puzzle_1 := Puzzle1(inputs)
+		fmt.Println(puzzle_1)
+
+		// puzzle_2 := Puzzle2(inputs)
+		// fmt.Println(puzzle_2)
+
+	})
+
+	c.Visit("https://adventofcode.com/2023/day/20/input")
 }
